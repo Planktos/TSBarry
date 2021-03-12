14edb97832d9b79603607a53204d897c3245df77
Planktos <kellylynnrobinson@gmail.com>
2021-03-12 12:18:38 -0600

    installing different packages trying to get 2019 storm data

--- cyclone_tracks.R
+++ cyclone_tracks.R
@@ -1,8 +1,21 @@
-#example Hurricane Track code
 
+# PURPOSE: Generate hurricane track for Tropical Storm Barry.
+
+# MODIFIED: 2021-03-12
+  # 2021-03-12 | KR modified from template cyclone track provided by Zach Topor
+
+# ---------
+
+# Hurricane Track code
+# packages -------
+#"hurricaneexposure"
 install.packages('hurricaneexposure')
 devtools::install_github('eddelbuettel/drat', force = T)
 library(drat)
+
+# "huricaneexposure" only has storms up to 2018; trying a dependancy "noaastormevents"
+library(noaastormevents)
+
 install.packages('stormwindmodel')
 addRepo("geanders")
 install.packages("hurricaneexposuredata")
@@ -10,7 +23,10 @@ library(tidyverse)
 library(stormwindmodel)
 library(hurricaneexposuredata)
 library(hurricaneexposure)
-ht <- data('hurr_tracks')
+
+# -------------
+data('hurr_tracks')
+ht <- hurr_tracks
 # hurricaneexposuredata::
 #
 # if (requireNamespace("hurricaneexposuredata", quietly = TRUE)) {
